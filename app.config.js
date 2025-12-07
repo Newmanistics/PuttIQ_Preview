50916ee3e47de0fe61143809083bcfed713de20d
ChrisScanNeo <chris@scanneo.co.uk>
2025-12-07 14:31:15 +0000

    chore: bump version to 2.1.2 and update build number to 37

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "PuttIQ",
     slug: "PuttIQ2",
-    version: "2.1.1",
+    version: "2.1.2",
     orientation: "landscape",
     icon: "./assets/icons/iTunesArtwork@2x.png",
     userInterfaceStyle: "light",
@@ -19,7 +19,7 @@ export default {
       requireFullScreen: false,
       orientation: "landscape",
       bundleIdentifier: "com.golfingiq.puttiq",
-      buildNumber: "36",
+      buildNumber: "37",
       runtimeVersion: {
         policy: "appVersion"
       },
